89bfbf3f60247b414cd059c566324f302af9f69d
Innocent1312 <anthonydev33@gmail.com>
2020-01-02 20:55:28 +0200

    favicon changed

--- src/App.js
+++ src/App.js
@@ -1,15 +1,10 @@
 import React from 'react';
 import './styles/bootstrap.min.css'
 import './styles/main.scss'
-import SwapiService from './api/SwapiService';
 import Header from './components/Header';
 import ItemList from './components/ItemList';
 import PersonDetails from './components/PersonDetails';
 import RandomPlanet from './components/RandomPlanet';
-
-const swapi = new SwapiService();
-
-swapi.getPerson(3).then(p => {console.log(p.name)});
 
 const App = () => {
   return (
